e4df78902b81fb08978ebe884e774d6388f6853a
huixu <huixu@depingroup.com>
2020-01-07 15:16:03 +0800

    更新版本

--- pages/zzwg/shza/index/index.js
+++ pages/zzwg/shza/index/index.js
@@ -1,12 +1,14 @@
-// pages/zhcg-cgwt/zhcg-cgwt.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-    currentTab: 0
+    currentTab: 0,
+    list: null,
+    list1: null,
+    list2: null
   },
 
   /**
@@ -14,12 +16,56 @@ Page({
    */
   onLoad: function (options) {
 
+    wx.request({
+      url: app.globalData.domain + 'api/SSEvent/getSSEventForNetGrid?page=1&pageSize=10',
+      method: 'post',
+      success: res => {
+        console.log("治安事件数据")
+        console.log(res.data)
+        this.setData({
+          list: this.list = res.data.data.vos
+        })
+        fail: res => {
+          console.log(res)
+        }
+      }
+    });
+    wx.request({
+      url: app.globalData.domain + 'api/Nonpublic/getNonPublicEconomyOrgInfo?page=1&pageSize=10',
+      method: 'post',
+      success: res => {
+        console.log("物流企业数据")
+        console.log(res.data)
+        this.setData({
+          list1: this.list1 = res.data.data.nonPublicEconomyOrgInfo
+        })
+        fail: res => {
+          console.log(res)
+        }
+      }
+    });
+    wx.request({
+      url: app.globalData.domain + 'api/Nonpublic/getNonPublicEconomyOrgInfo?page=1&pageSize=10',
+      method: 'post',
+      success: res => {
+        console.log("重点企业数据")
+        console.log(res.data)
+        this.setData({
+          list2: this.list2 = res.data.data.nonPublicEconomyOrgInfo
+        })
+        fail: res => {
+          console.log(res)
+        }
+      }
+    })
+
   },
   /**
    * 滑动切换tab
    */
   bindChange: function (e) {
     var that = this;
+
     that.setData({
       currentTab: e.detail.current
     });
--- pages/zzwg/zzgl/index/index.js
+++ pages/zzwg/zzgl/index/index.js
@@ -1,12 +1,13 @@
-// pages/zhcg-cgwt/zhcg-cgwt.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-    currentTab: 0
+    currentTab: 0,
+    list: null,
+    list1: null
   },
 
   /**
@@ -14,12 +15,43 @@ Page({
    */
   onLoad: function (options) {
 
+    wx.request({
+      url: app.globalData.domain + 'Hidden/getHiddenList',
+      method: 'post',
+      data: { "pageSize": "10", "start": "0", "state": "01" },
+      success: res => {
+        console.log("待处理数据")
+        console.log(res.data)
+        this.setData({
+          list: this.list = res.data.rows
+        })
+        fail: res => {
+          console.log(res)
+        }
+      }
+    });
+    wx.request({
+      url: app.globalData.domain + 'Hidden/getHiddenList',
+      method: 'post',
+      data: { "pageSize": "10", "start": "0", "state": "02" },
+      success: res => {
+        console.log("已处理数据")
+        console.log(res.data)
+        this.setData({
+          list1: this.list1 = res.data.rows
+        })
+        fail: res => {
+          console.log(res)
+        }
+      }
+    })
   },
   /**
    * 滑动切换tab
    */
   bindChange: function (e) {
     var that = this;
+
     that.setData({
       currentTab: e.detail.current
     });
